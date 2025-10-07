f48f776babd6a7b6ec5a78ce99e730c10b471f8c
sparkitga <sparkitga@gmail.com>
2025-10-07 05:50:37 -0400

    delete

--- components/menu-hero.tsx
+++
@@ -1,7 +0,0 @@
-export function MenuHero() {
-  return (
-    <section className="py-12 text-center">
-      <h1 className="text-4xl tracking-[0.2em] text-[#a89968] font-light mb-2">BOX OF THE BEST</h1>
-    </section>
-  )
-}
